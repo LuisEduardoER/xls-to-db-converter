596c91fdc10bdcbfb6a48b7f4e0805f6bb1ec734
alex91.ya <alex91.ya@gmail.com>
2013-06-17 07:38:43 +0000

    Add a new exception: UnsupportedFormatOfInputFileException

---
+++ ExcelConverter/src/exceptions/UnsupportedFormatOfInputFileException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class UnsupportedFormatOfInputFileException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public String toString() {
+		return "Структура файла не соответствует установленной";
+	}
+
+}
