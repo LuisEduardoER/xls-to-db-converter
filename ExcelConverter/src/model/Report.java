3b3c2d2dcbf48e06038cf86c6b26e09cd46267f0
alex91.ya <alex91.ya@gmail.com>
2013-06-18 20:22:58 +0000

    Change Reporter to Report and add some changes to it.

---
+++ ExcelConverter/src/model/Report.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Report {
+
+	private FileWriter fileWriter;
+
+	public Report(String filename) throws IOException {
+		try {
+			fileWriter = new FileWriter(filename);
+		} catch (IOException e) {
+			System.out.println("Ошибка при создании файла отчета");
+			throw e;
+		}
+	}
+
+	/**
+	 * Close the file and save changes
+	 * 
+	 * @throws IOException
+	 */
+	public void closeFile() throws IOException {
+		try {
+			fileWriter.close();
+		} catch (IOException e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * Write string to the file and return carriage
+	 * 
+	 * @param text
+	 *            output string
+	 * @throws IOException
+	 */
+	public void writeln(String text) throws IOException {
+		fileWriter.write(text + "\n");
+	}
+
+	/**
+	 * Write string to the file
+	 * 
+	 * @param text
+	 *            output string
+	 * @throws IOException
+	 */
+	public void write(String text) throws IOException {
+		fileWriter.write(text);
+	}
+
+	/**
+	 * Close the file (if it wasn't) when collector will work
+	 * 
+	 * @throws IOException
+	 */
+	protected void finalize() throws IOException {
+		if (fileWriter != null)
+			closeFile();
+	}
+}
