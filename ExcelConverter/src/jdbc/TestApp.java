95a88dcd8ad6d6142b80aaf723309baeeb2872db
Ralf17031992@gmail.com <Ralf17031992@gmail.com@e82e5fba-4e68-8c33-0286-b43e4b3b72b8>
2013-06-19 08:53:02 +0000

    Changes

--- ExcelConverter/src/jdbc/TestApp.java
+++ ExcelConverter/src/jdbc/TestApp.java
@@ -16,8 +16,7 @@ public class TestApp {
 	 */
 	public static void main(String[] args) throws IOException {
 		String orgType[] = { "ООО", "ИП", "ЧУП", "ОАО", "ИООО" };
-		String street[] = { "Пушкинская ", "Московская ", "Мичурина ",
-				"Гоголя ", "Ленина ", "Советская " };
+		String street[] = { "Пушкинская ", "Московская ", "Мичурина ", "Гоголя ", "Ленина ", "Советская " };
 		Random rand = new Random();
 		LinkedList<SheetLine> l = new LinkedList<SheetLine>();
 		SheetLine l4[] = new SheetLine[10];
@@ -32,9 +31,9 @@ public class TestApp {
 			sl.setType(orgType[rand.nextInt(5)]);
 			sl.setName("Организация " + rand.nextInt(1000));
 			sl.setAddress(street[rand.nextInt(6)] + rand.nextInt(300));
-			sl.setUnp(rand.nextInt(1000000000));
-			sl.setOkpo(rand.nextInt(1000000000));
-			sl.setAccount(rand.nextInt(1000000000));
+			sl.setUnp(100000000 + rand.nextInt(89999999));
+			sl.setOkpo(100000000000l + rand.nextInt(99999999));
+			sl.setAccount(2000000000000l + rand.nextInt(99999999));
 			sl.setNets(rand.nextInt(2) == 1 ? true : false);
 			l.add(sl);
 		}
@@ -47,9 +46,9 @@ public class TestApp {
 		l1.setType("ООО");
 		l1.setName("Повтор incorporation");
 		l1.setAddress("ул. Пушкинская 1");
-		l1.setUnp(10101010);
-		l1.setOkpo(2020202020l);
-		l1.setAccount(3030303030l);
+		l1.setUnp(101010100);
+		l1.setOkpo(202020202000l);
+		l1.setAccount(3030303030000l);
 		l1.setNets(false);
 		l.add(l1);
 
@@ -64,8 +63,7 @@ public class TestApp {
 			db.sendToDB(l, r);
 			db.disconnect();
 		} catch (DataBaseException e) {
-			System.out
-					.println("Сработало исключение DataBaseException. Вывод в файл остановлен!");
+			System.out.println("Сработало исключение DataBaseException. Вывод в файл остановлен!");
 			System.out.println("Причина: " + e.getMessage());
 		} catch (IOException e) {
 			System.out.println("Ошибка записи данных в файл отчета");
